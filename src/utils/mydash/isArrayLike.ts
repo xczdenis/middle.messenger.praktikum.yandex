217ce3c98313cbb92e2b8050ad50a8e8df628314
Denis <dfgaraev@yandex.ru>
2022-04-25 20:35:13 +0300

    Sprint 2

--- src/utils/mydash/isArrayLike.ts
+++
@@ -1,8 +0,0 @@
-// import isNil from './isNil'
-// import isLength from './isLength'
-//
-// function isArrayLike<T>(value: T): boolean {
-//   return !isNil(value) && typeof value !== 'function' && isLength(value.length)
-// }
-//
-// export default isArrayLike
