05dbe19f2e985deb4952feb0ce3fff9e281d5c1d
Denis <dfgaraev@yandex.ru>
2022-04-26 14:19:48 +0300

    Sprint 2

---
+++ src/components/List/template.ts
@@ -0,0 +1,9 @@
+export const template = `
+<h1>{{header}}</h1>
+<ul>
+  <li>1</li>
+  <li>2</li>
+  <li>3</li>
+</ul>
+<div {{{child 'BtnSend'}}}></div>
+`
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import { app } from './app'
 import './assets/less/main.less'
 import 'remixicon/fonts/remixicon.css'
+import { Component as List } from './components/List/list'
 
 document.addEventListener('DOMContentLoaded', () => {
   app().mount('#app')
+  const cmp = new List()
+  // console.log(cmp)
+  // console.log(cmp.render())
+  cmp.render()
+  // cmp.header = 'dsf'
+  // setTimeout(() => {
+  //   cmp.header = 'Пока'
+  // }, 1000)
 })
